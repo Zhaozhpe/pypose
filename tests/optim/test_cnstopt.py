94c516dd1872967772c67c3d7ed821dd9b3c19e4
Zhaozhpe <zhaozhp2001@gmail.com>
2024-05-08 11:47:55 -0400

    typo

--- tests/optim/test_cnstopt.py
+++ tests/optim/test_cnstopt.py
@@ -10,7 +10,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class TestOptim:
 
-    def test_tensor(slef):
+    def test_tensor():
         class TensorModel(nn.Module):
             def __init__(self, *dim) -> None:
                 super().__init__()
